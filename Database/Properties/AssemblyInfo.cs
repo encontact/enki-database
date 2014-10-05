b18ab2ac5d8bbef4939474bc47286883093edbdb
Reinaldo Coelho Sartorelli <reinaldo.coelho@gmail.com>
2014-10-04 23:24:14 -0300

    Adicionado pacote Nuget
    Removidos testes que dependem de script de BD

--- Database/Properties/AssemblyInfo.cs
+++ Database/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Database")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Enkilabs database access")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Enkilabs")]
 [assembly: AssemblyProduct("Database")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("Enkilabs")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
